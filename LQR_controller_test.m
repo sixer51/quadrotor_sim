826ab56933535154a34889566d1f4f30688049e5
sixer51 <sixerwyj@gmail.com>
2024-04-28 18:42:09 -0400

    would follow the uav

--- LQR_controller_test.m
+++ LQR_controller_test.m
@@ -14,16 +14,21 @@ sigma = 0.01;  % The proportionality constant relating thrust to torque [m]
 quad = quadrotor(g, l, m, diag(I), mu, sigma);
 
 % INTRUDER
+% circle path
 % path = @(t) [cos(t); sin(t); 2];
-% dist = struct("r", @(t,z)0.1*[sin(t); sin(2*t); sin(4*t)],...
-%     "n", @(t,z) 0.1*[0.1; 0.01; 0.1]);
 
 % straight line path
 % 0.1+0.2*t
-path = @(t) [1; 0; t];
-dist = struct(...
-    "r", @(t,z) 0.1 * [0; 0; sin(t)],...
-    "n", @(t,z) 0.1 * [0.1; 0.01; 0.1]); 
+% path = @(t) [-5+0.2*t; cos(t); 3];
+% path = @(t) [1; 1; t];
+
+% path = @(t) [-2+0.2*t; -5+0.1*t; 2+0.2*t];
+
+% staright, horizontal movement
+path = @(t) [-5+0.5*t; 1; 3];
+
+dist = struct("r", @(t,z)0.1*[sin(t); sin(2*t); sin(4*t)],...
+    "n", @(t,z) 0.1*[0.1; 0.01; 0.1]);
  
 intruder = uav(path, dist);
 
@@ -44,33 +49,69 @@ z0 = zeros(12,1);
 % ANIMATION
 sim.animate(t, z, y);
 
-% Plotting
-figure(2)
-for i=1:4
-    ax(i) = subplot(2,2,i,'NextPlot','Add','Box','on','XGrid','on','YGrid','on',...
+% Plotting quadrotor states
+% figure(2)
+% for i=1:4
+%     ax(i) = subplot(2,2,i,'NextPlot','Add','Box','on','XGrid','on','YGrid','on',...
+%                 'Xlim',[t(1), t(end)],...
+%                 'TickLabelInterpreter','LaTeX','FontSize',14);
+%     xlabel('t','Interpreter','LaTeX','FontSize',14);        
+% end
+% 
+% 
+% plot(ax(1), t,z(:,1:3), 'LineWidth', 1.5);
+% legend(ax(1), {'$x_1$', '$x_2$', '$x_3$'},... 
+%     'Interpreter', 'LaTeX', 'FontSize', 14);
+% title(ax(1), '${\bf x}$','Interpreter','LaTeX','FontSize',14);
+% xlabel(ax(1), 't','Interpreter','LaTeX','FontSize',14);
+% 
+% plot(ax(3), t, z(:,4:6), 'LineWidth', 1.5);
+% legend(ax(3), {'$\phi$', '$\theta$', '$\psi$'},...
+%     'Interpreter', 'LaTeX', 'FontSize', 14);
+% title(ax(3), '\boldmath$\alpha$','Interpreter','LaTeX','FontSize',14);
+% 
+% plot(ax(2), t, z(:,7:9), 'LineWidth', 1.5);
+% legend(ax(2), {'$\dot{x}_1$', '$\dot{x}_2$', '$\dot{x}_3$'},...
+%     'Interpreter', 'LaTeX', 'FontSize', 14);
+% title(ax(2), '$\dot{\bf x}$','Interpreter','LaTeX','FontSize',14);
+% 
+% plot(ax(4), t, z(:,10:12), 'LineWidth', 1.5);
+% legend(ax(4), {'$\omega_1$', '$\omega_2$', '$\omega_3$'},...
+%     'Interpreter', 'LaTeX', 'FontSize', 14);
+% title(ax(4), '\boldmath$\omega$','Interpreter','LaTeX','FontSize',14);
+
+% Plotting quadrotor states
+figure()
+for i=1:6
+    ax(i) = subplot(3,2,i,'NextPlot','Add','Box','on','XGrid','on','YGrid','on',...
                 'Xlim',[t(1), t(end)],...
                 'TickLabelInterpreter','LaTeX','FontSize',14);
     xlabel('t','Interpreter','LaTeX','FontSize',14);        
 end
 
 
-plot(ax(1), t,z(:,1:3), 'LineWidth', 1.5);
-legend(ax(1), {'$x_1$', '$x_2$', '$x_3$'},... 
+plot(ax(1), t,[z(:,1), y(:,1)], 'LineWidth', 1.5);
+legend(ax(1), {'$x_{quad}$', '$x_{uav}$'},... 
     'Interpreter', 'LaTeX', 'FontSize', 14);
 title(ax(1), '${\bf x}$','Interpreter','LaTeX','FontSize',14);
 xlabel(ax(1), 't','Interpreter','LaTeX','FontSize',14);
 
-plot(ax(3), t, z(:,4:6), 'LineWidth', 1.5);
-legend(ax(3), {'$\phi$', '$\theta$', '$\psi$'},...
+plot(ax(3), t, [z(:,2), y(:,2)], 'LineWidth', 1.5);
+legend(ax(3), {'$y_{quad}$', '$y_{uav}$'},...
     'Interpreter', 'LaTeX', 'FontSize', 14);
-title(ax(3), '\boldmath$\alpha$','Interpreter','LaTeX','FontSize',14);
+title(ax(2), '${\bf y}$','Interpreter','LaTeX','FontSize',14);
 
-plot(ax(2), t, z(:,7:9), 'LineWidth', 1.5);
-legend(ax(2), {'$\dot{x}_1$', '$\dot{x}_2$', '$\dot{x}_3$'},...
+plot(ax(5), t, [z(:,3), y(:,3)], 'LineWidth', 1.5);
+legend(ax(5), {'$z_{quad}$', '$z_{uav}$'},...
     'Interpreter', 'LaTeX', 'FontSize', 14);
-title(ax(2), '$\dot{\bf x}$','Interpreter','LaTeX','FontSize',14);
+title(ax(2), '$${\bf z}$','Interpreter','LaTeX','FontSize',14);
 
-plot(ax(4), t, z(:,10:12), 'LineWidth', 1.5);
-legend(ax(4), {'$\omega_1$', '$\omega_2$', '$\omega_3$'},...
-    'Interpreter', 'LaTeX', 'FontSize', 14);
-title(ax(4), '\boldmath$\omega$','Interpreter','LaTeX','FontSize',14);
+plot(ax(2), t,z(:,1)- y(:,1), 'LineWidth', 1.5);
+title(ax(2), '${\bf x error}$','Interpreter','LaTeX','FontSize',14);
+xlabel(ax(2), 't','Interpreter','LaTeX','FontSize',14);
+
+plot(ax(4), t, z(:,2)- y(:,2), 'LineWidth', 1.5);
+title(ax(4), '${\bf y error}$','Interpreter','LaTeX','FontSize',14);
+ 
+plot(ax(6), t, z(:,3)- y(:,3), 'LineWidth', 1.5);
+title(ax(6), '$${\bf z error}$','Interpreter','LaTeX','FontSize',14);
--- src/lqr_controller.m
+++ src/lqr_controller.m
@@ -2,7 +2,7 @@ classdef lqr_controller < handle
     properties(Access = public)
         % altitude(1,1) double;
         % k(1,2) double;
-        u0(1,1) double;
+        u0(4, 1) double;
         A(12, 12) double;
         B(12, 4) double;
         Q(12, 12) double;
@@ -12,9 +12,7 @@ classdef lqr_controller < handle
 
     methods(Access = public)
         function obj = lqr_controller(quad)
-            obj.u0 = quad.m*quad.g/4;
-            % obj.altitude = altitude;
-            % obj.k = gains;
+            obj.u0 = quad.m*quad.g/4 * ones(4, 1);
 
             obj.A = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0;
                      0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0;
@@ -43,26 +41,126 @@ classdef lqr_controller < handle
             % disp(obj.A);
             % disp(obj.B);
 
-            C = diag([1000,1000, 1000, 100, 100, 100, 100, 100, 100, 10000, 10000, 10000]);
-            obj.Q = C.' * C;
-            % obj.Q = diag([1000,1000, 1000, 100, 100, 100, 100, 100, 100, 10000, 10000, 10000]);
-            obj.R = eye(4) * 0.001;
+            % C = diag([1000, 1000, 1000, 100, 100, 100, 100, 100, 100, 10000, 10000, 10000]);
+            % obj.Q = C.' * C;
+            % % obj.Q = diag([100,100, 1000, 100, 100, 100, 100, 100, 100, 1000, 1000, 1000]);
+            % obj.R = eye(4)*0.001;
 
+            % C = diag([1, 1, 10, 1, 1, 1, 1, 1, 1, 10, 10, 10]);
+            % obj.Q = C.' * C;
+            % obj.Q = diag([50,50, 500, 0.1, 0.1, 0.01, 1, 1, 1, 1, 1,
+            % 0.01]); work for some straight line
+            % obj.Q = diag([50,50, 500, 0.1, 0.1, 0.01, 1, 1, 1, 1, 1, 0.01]);
+            
+            % bryson's rule
+            obj.Q = diag([10,10, 5, 100, 100, 100, 1, 1, 1, 1000, 1000, 1000]);
+            obj.R = eye(4) * 1/(quad.mu^2);
+
+            % obj.Q = diag([0.1, 0.1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+            % obj.R = eye(4);
+
+            % obj.Q = diag([1,1, 5, 0.1,0.1, 0.1, 1,1,3,1,1, 0.1]);
+            % obj.R = eye(4) * 10;
+
+            % obj.Q = diag([1000,1000, 1000, 100, 100, 100, 100, 100, 100, 10000, 10000, 10000]);
+            % obj.R = eye(4)*0.001;
+
+            % obj.Q = [0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+            %      0, 0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+            %      0, 0, 0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+            %      0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0;
+            %      0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0;
+            %      0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0;
+            %      0, 0, 0, 0, 0, 0, 0.0, 0, 0, 0, 0, 0;
+            %      0, 0, 0, 0, 0, 0, 0, 0.0, 0, 0, 0, 0;
+            %      0, 0, 0, 0, 0, 0, 0, 0, 0.0, 0, 0, 0;
+            %      0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0;
+            %      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0;
+            %      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+            % obj.R = eye(4);
         end
 
         function u = output(obj, iscaptured, z, y)
-            % u = obj.u0 + repmat(obj.k*[(obj.altitude - z(3)); -z(9)],[4,1]);
-            [K, S, P] = lqr(obj.A, obj.B, obj.Q, obj.R);
-            % disp(K);
-            % u = obj.u0 * [1;1;1;1]; % + K(:, 1:12) * ()
+            Q_captured = diag([10,10, 100, 1000, 1000, 200, 1, 1, 5, 1000, 1000, 1000]);
+
+            % good for horizontal straight line
+            % Q_far = diag([10,10, 10, 100, 100, 100, 1, 1, 1, 1000, 1000, 1000]);
+            % Q_conservative = diag([200,200, 100, 200, 200, 200, 1, 1, 1, 1000, 1000, 1000]);
+            % Q_close = diag([1000,1000, 100, 500, 500, 500, 1, 1, 1, 1000, 1000, 1000]);
             
-            if iscaptured
+            % tuning for circle
+            Q_far = diag([10,10, 10, 100, 100, 100, 1, 1, 5, 1000, 1000, 1000]);
+            Q_conservative = diag([100,100, 100, 200, 200, 200, 1, 1, 5, 1000, 1000, 1000]);
+            % Q_close = diag([200,200, 2000, 500, 500, 500, 1, 1, 1, 1000, 1000, 1000]);
+            Q_close = diag([200,200, 200, 500, 500, 500, 1, 1, 1, 1000, 1000, 1000]);
+            % Q_very_close = diag([1000,1000, 100, 200, 200, 200, 0.1, 0.1, 0.1, 1000, 1000, 1000]);
+
+            % Q_far = diag([10,10, 10, 100, 100, 100, 0.001, 0.001, 0.001, 1000, 1000, 1000]);
+            % Q_conservative = diag([100,100, 100, 200, 200, 200, 0.001, 0.001, 0.001, 1000, 1000, 1000]);
+            % Q_close = diag([200,200, 200, 200, 200, 200, 0.001, 0.001, 0.001, 1000, 1000, 1000]);
+            
+
+            is_inside_nest = false;
+            cage_size = 10;
+            x_ok = z(1) < cage_size/2 && z(1) > -cage_size/2;
+            y_ok = z(2) < cage_size/2 && z(2) > -cage_size/2;
+            z_ok = z(3) < cage_size && z(3) >= 0;
+            if x_ok && y_ok && z_ok
+                is_inside_nest = true;
+            end
+
+            if iscaptured || ~is_inside_nest
                 % move to certain position
-                u = -K * (z - [[0;0;0]; zeros(9, 1)]);
+                % disp("back to home");
+                zd = [[0;0;1]; zeros(9, 1)];
+                z_error = z - zd;
             else
                 % catch uav
-                u = -K * (z - [y; zeros(9, 1)]);
+                zd = [y; zeros(9, 1)];
+                z_error = z - zd;
+                % v = normalize(z_error(1:3))/10.;
+                % z_error(7:9) = -v;
             end
+            % fake gravity compensation
+            z_error(3) = z_error(3)-0.1;
+            
+            has_aggresive_rotation = false;
+            if abs(z(4)) > 0.3 || abs(z(5)) > 0.3
+                has_aggresive_rotation = true;
+            end
+            
+            Q_curr = Q_conservative;
+            if iscaptured
+                Q_curr = Q_captured;
+            elseif abs(z_error(1)) > 2 || abs(z_error(2)) > 2 || abs(z_error(3)) > 2
+                Q_curr = Q_far;
+            elseif (abs(z_error(1)) < 0.8 || abs(z_error(2)) < 0.8 || abs(z_error(3)) < 0.8) && ~has_aggresive_rotation
+                % z_error(7:9) = -z_error(1:3)/1000.;
+                % z_error(7:9) = z(7:9)-z_error(1:3);
+                % disp(z_error(7:9));
+                % disp("close");
+                Q_curr = Q_close;
+            % elseif (abs(z_error(1)) < 0.3 || abs(z_error(2)) < 0.3 || abs(z_error(3)) < 0.3) && ~has_aggresive_rotation
+            %     % z_error(7:9) = z(7:9)-z_error(1:3);
+            %     % disp(z_error(7:9));
+            %     % disp("very close");
+            %     Q_curr = Q_very_close;
+            end
+
+            [K, ~, ~] = lqr(obj.A, obj.B, Q_curr, obj.R);
+
+            % u = -K * (z - [[-4.8;1;3]; zeros(9, 1)]);
+            % u = -K * (z - [y; zeros(9, 1)]);
+            u = -K * z_error;
+            
+            % if iscaptured || ~is_inside_nest
+            %     % move to certain position
+            %     disp("outside of the nest");
+            %     u = -K * (z - [[0;0;1]; zeros(9, 1)]); % + obj.u0;
+            % else
+            %     % catch uav
+            %     u = -K * (z - [y; zeros(9, 1)]); % + obj.u0;
+            % end
             
 
         end
